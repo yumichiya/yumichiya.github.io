73da0427a3b5a898a4e7025adbaa9044f482c37b
Jasmine Nguyen <yukariaimi@gmail.com>
2020-02-25 17:06:05 -0600

    Test 1

---
+++ test.js
@@ -0,0 +1,52 @@
+(function() {
+    function login(callback) {
+        var client_id = '4977f5049db14ed99184bd454c05716a';
+        var redirect_uri = 'https://yumichiya.github.io';
+        function getLoginURL(scopes) {
+            return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
+            '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+            '&scope=' + encodeURIComponent(scopes.join(' ')) +
+            '&response_type=token';
+        }
+
+        var url = getLoginURL(['user-read-email']);
+
+        var width = 450,
+            height = 730, 
+            left = (screen.width / 2) - (width / 2), 
+            top = (screen.height / 2) - (height / 2);
+        
+        window.addEventListener("message", function(event) {
+            var hash = JSON.parse(event.data);
+            if (hash.type == 'access_token') {
+                callback(hash.access_token);
+            }
+        }, false);
+
+        var w = window.open(url, 'Spotify', 'menubar=no,location=no,resizeable=no,scrollbars=no,status=no,width=' + width + ', height=' + height + ',top=' + top + ',left=' + left);
+    }
+
+        function getUserData(accessToken) {
+            return $.ajax({
+                url: 'https://api.spotify.com/v1/me',
+                headers: {
+                   'Authorization': 'Bearer ' + accessToken
+                }
+            });
+        }
+    
+        var templateSource = document.getElementById('user-profile-template').innerHTML,
+            template = Handlebars.compile(templateSource),
+            resultsPlaceholder = document.getElementById('user-profile'),
+            loginButton = document.getElementById('login-button');
+        
+        loginButton.addEventListener('click', function() {
+            login(function(accessToken) {
+                getUserData(accessToken)
+                    .then(function(response) {
+                        loginButton.style.display = 'none';
+                        resultsPlaceholder.innerHTML = template(response);
+                    });
+                });
+        });
+})();
