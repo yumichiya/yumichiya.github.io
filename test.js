c121c40da491681ac4e84d1e091e2c44f220d75d
Jasmine Nguyen <yukariaimi@gmail.com>
2020-02-25 21:48:34 -0600

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
     
     function login(callback) {
         var CLIENT_ID = '4977f5049db14ed99184bd454c05716a';
-        var REDIRECT_URI = 'https://yumichiya.github.io';
+        var REDIRECT_URI = 'https://yumichiya.github.io/';
         function getLoginURL(scopes) {
             return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
               '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
@@ -26,6 +26,10 @@
             }
         }, false);
         
+        var w = window.open(url,
+                            'Spotify',
+                            'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
+                           );
         
     }
 
@@ -38,17 +42,16 @@
         });
     }
 
-    var templateSource = document.getElementById('user-profile-template').innerHTML,
+    var templateSource = document.getElementById('result-template').innerHTML,
         template = Handlebars.compile(templateSource),
-        resultsPlaceholder = document.getElementById('user-profile'),
-        loginButton = document.getElementById('login-button'),
-        loggedin = document.getElementById('loggedin');
+        resultsPlaceholder = document.getElementById('result'),
+        loginButton = document.getElementById('btn-login');
         
     
     loginButton.addEventListener('click', function() {
         login(function(accessToken) {
             getUserData(accessToken).then(function(response) {
-                    console.log('yes');
+                    console.log(response);
                     loginButton.style.display = 'none';
                     resultsPlaceholder.innerHTML = template(response);
                     loggedin.style.display = 'block';
