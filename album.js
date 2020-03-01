d2a7d4875b73dcc42790f8bfef68a993b85691e0
Jasmine Nguyen <yukariaimi@gmail.com>
2020-02-29 20:23:30 -0600

    Test 1

--- album.js
+++ album.js
@@ -1,9 +1,9 @@
 var request = require("request");
 var user_id = "test";
 var token = "Bearer";
-var playlist_url = "https://api.spotify.com/v1/user/"+user-id+"/playlists";
+var playlist_url = "https://api.spotify.com/v1/user/"+user_id+"/playlists";
 
-request({url:playlist_url, headers:{"Authorization":token}}, function(err,res){ //err = error, res = result
+request({url:playlist_url, headers:{"Authorization":token}}, function(err,res){ //err = error, res = result 
     if(res){
         var playlists=JSON.parse(res.body);
         varplaylist_url = playlists.item[0].href
