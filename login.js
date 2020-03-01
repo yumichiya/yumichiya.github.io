6fd4c4cd7df377fd98b2fd87df9dec17d8afbae3
Jasmine Nguyen <yukariaimi@gmail.com>
2020-02-29 20:44:47 -0600

    Update login.js

--- login.js
+++ login.js
@@ -87,24 +87,17 @@
         window.location = url;
       }, false);
     }
-    
-    var user_id = "test";
-    var playlist_url = "https://api.spotify.com/v1/user/"+user_id+"/playlists";
 
-    request({url:playlist_url, headers:{"Authorization":access_token}}, function(err,res){ //err = error, res = result 
-        if(res){
-            var playlists=JSON.parse(res.body);
-            varplaylist_url = playlists.item[0].href
-            request({url:playlist_url, headers:{"Authorization":access_token}}, function(err,res){
-                if(res){
-                    var playlist = JSON.parse(res.body);
-                    console.log("playlist: " + playlist.name);
-                    playlist.tracks.forEach(function(track){
-                        console.log(track.track.name);
-                    })
-                }
-            })
-        }
-    })
+    document.getElementById('login-button').addEventListener('click', function() {
+        $.ajax({
+            url: 'https://api.spotify.com/v1/browse/featured-playlists',
+            headers: {
+              'Authorization': 'Bearer ' + access_token
+            },
+            success: function(response) {
+              console.log('working');
+            }
+        });
+    }, false);
 
 })();
