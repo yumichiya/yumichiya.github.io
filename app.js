b86ed09afe76a43dbd12b852606349149ed14f19
Jasmine Nguyen <yukariaimi@gmail.com>
2020-03-25 14:48:05 -0500

    fixed auth

--- app.js
+++ app.js
@@ -103,7 +103,7 @@
 
       // The app redirect uri 
       //change to http://localhost:3000/ to test and https://yumichiya.github.io/ to deploy
-      var redirect_uri = 'https://yumichiya.github.io/'; 
+      var redirect_uri = 'http://localhost:3000/'; 
 
       var state = generateRandomString(16);
 
