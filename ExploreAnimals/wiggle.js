7cbd32714b6de2a2fbe8c37dd6b1a0e3e7389826
Jasmine Nguyen <jasminenhi3@gmail.com>
2022-01-04 09:20:29 -0600

    Reverting to old site

---
+++ ExploreAnimals/wiggle.js
@@ -0,0 +1,62 @@
+function killMe() {
+    var lion = document.getElementById("lion");
+    lion.play();
+}
+
+function elephantAudio(){
+    var ele = document.getElementById("ele");
+    ele.play();
+}
+
+/*ALTERNATE THESE TWO! -----------------------------------------------------------------------------------*/
+function kittenAudio() {
+    var purr = document.getElementById("purr");
+    purr.play();
+    var meow = document.getElementById("meow");
+    meow.play();
+}
+
+function ribbitAudio() {
+    var ribbit = document.getElementById("ribbit");
+    ribbit.play();
+}
+
+function monkeyAudio() {
+    var monkey = document.getElementById("monkey");
+    monkey.play();
+}
+
+function dogAudio() {
+    var woof = document.getElementById("woof");
+    woof.play();
+}
+
+function pigAudio() {
+    var oink = document.getElementById("oink");
+    oink.play();
+}
+
+function sheepAudio() {
+    var baa = document.getElementById("baa"); 
+    baa.play();
+}
+
+function chickenAudio() {
+    var pollo = document.getElementById("pollo");
+    pollo.play();
+}
+
+function cowAudio() {
+    var moo = document.getElementById("moo");
+    moo.play();
+}
+
+function bearAudio() {
+    var bear = document.getElementById("bear");
+    bear.play();
+}
+
+function owlAudio() {
+    var who = document.getElementById("who");
+    who.play();
+}
