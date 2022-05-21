40dd45ea3ff5b8f442ab0bc646b241326c2b18bd
Jasmine Nguyen <yukariaimi@gmail.com>
2022-05-21 01:27:00 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,29 +1,29 @@
-let home = document.getElementById("home");
-let about = document.getElementById("about");
-let works = document.getElementById("works");
+// let home = document.getElementById("home");
+// let about = document.getElementById("about");
+// let works = document.getElementById("works");
 
 
-function menu() {
-   document.getElementById("menuIcon").classList.toggle("change");
-   document.getElementById("menu").classList.toggle("showMenu");
-   let container = document.getElementsByClassName('showMenu')[0];
-   container.addEventListener('click', function( event ) {
-      console.log(event.target.className);
-      if (event.target.className == 'home') {
-            home.style.display = "flex";
-            home.style.height = "100%";
+// function menu() {
+//    document.getElementById("menuIcon").classList.toggle("change");
+//    document.getElementById("menu").classList.toggle("showMenu");
+//    let container = document.getElementsByClassName('showMenu')[0];
+//    container.addEventListener('click', function( event ) {
+//       console.log(event.target.className);
+//       if (event.target.className == 'home') {
+//             home.style.display = "flex";
+//             home.style.height = "100%";
             
-      } else if (event.target.className == 'about') {
-         about.style.display = "flex";
-         about.style.height = "100%";
-      } else if (event.target.className == 'works') {
-         works.style.display = "flex";
-         works.style.height = "100%";
-      }
-      document.getElementById("menuIcon").classList.remove("change");
-      document.getElementById("menu").classList.remove("showMenu");
-   });
-}    
+//       } else if (event.target.className == 'about') {
+//          about.style.display = "flex";
+//          about.style.height = "100%";
+//       } else if (event.target.className == 'works') {
+//          works.style.display = "flex";
+//          works.style.height = "100%";
+//       }
+//       document.getElementById("menuIcon").classList.remove("change");
+//       document.getElementById("menu").classList.remove("showMenu");
+//    });
+// }    
 
 // function menu() {
 
