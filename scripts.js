029171daff2357c64b0096fb7b97c93738e4af3e
Jasmine Nguyen <yukariaimi@gmail.com>
2022-07-18 13:45:15 -0500

    Update scripts.js
    fixed scrolling feature

--- scripts.js
+++ scripts.js
@@ -3,29 +3,31 @@ let about = document.getElementById("about");
 let works = document.getElementById("works");
 
 let active = ["home", "about", "works"];
-
+let i = 0;
 
 function menu() {
    document.getElementById("menuIcon").classList.toggle("change");
    document.getElementById("menu").classList.toggle("showMenu");
    let container = document.getElementsByClassName('showMenu')[0];
    container.addEventListener('click', function( event ) {
-      console.log(event.target.className);
       if (event.target.className == 'home') {
          home.style.display = "flex";
          home.style.height = "100%";
          about.style.display = "none";
          works.style.display = "none";
+         i = 0;
       } else if (event.target.className == 'about') {
          about.style.display = "flex";
          about.style.height = "100%";
          home.style.display = "none";
          works.style.display = "none";
+         i = 1;
       } else if (event.target.className == 'works') {
          works.style.display = "flex";
          works.style.height = "100%";
          home.style.display = "none";
          about.style.display = "none";
+         i = 2;
       }
       document.getElementById("menuIcon").classList.remove("change");
       document.getElementById("menu").classList.remove("showMenu");
@@ -33,7 +35,7 @@ function menu() {
 }    
 
 
-let i = 0;
+
 document.addEventListener('wheel', scrollHandler);
 function scrollHandler(e) {
    if(e.deltaY == -100) {
